cd36faae5ce633bfdd068a42ae5bed45c778879d
cbgit <bernard.carl@gmail.com>
2013-07-07 01:32:05 +0000

    Updated web.js to reade from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs=require('fs');
 
+var input_file='index.html';
 var app = express.createServer(express.logger());
 
+var data_buffer= fs.readFileSync(input_file);
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+  var data_buffer= fs.readFileSync(input_file);
+  response.send(data_buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
